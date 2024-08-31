788f8ea06982bcc0b8b42c2d4e97802f78a1002f
anderDaZu <anderda-96@hotmail.com>
2024-08-30 21:16:46 -0500

    Tailwind components

--- src/modules/landing/pages/HomePage.vue
+++ src/modules/landing/pages/HomePage.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1 class="mx-auto font-bold text-lg text-center uppercase">Home Page</h1>
+    <div class="text-center">
+        <h1 class="font-bold text-4xl tracking-tighter md:text-5xl">Bienvenido a nuestro sitio web</h1>
+        <p class="mx-auto max-w-[600px] text-gray-500 md:text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quisquam accusantium 
+            perspiciatis atque possimus harum porro dolorum iste quasi in non asperiores corrupti, at, sit debitis optio impedit ullam sequi!
+        </p>
+    </div>
 </template>
 
 <script setup></script>
